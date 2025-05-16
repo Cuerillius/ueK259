2fabab3c707ed3cda6ced62d308815bf3bc59d68
Cuerillius <cyrill_weber@hotmail.com>
2025-05-16 07:53:50 +0200

    preformance analysis kmeans

--- kmeans.py
+++ kmeans.py
@@ -6,47 +6,8 @@ import numpy as np
 import math
 
 features, true_labels = make_blobs(
-    n_samples=500, centers=4, cluster_std=0.75, random_state=42
+    n_samples=500, centers=4, cluster_std=1.75, random_state=43
 )
-
-""" fig, ax = plt.subplots()
-ax.scatter(x=features[:, 0], y=features[:, 1], s=5, c=true_labels)
- """
-
-""" cluster_centers = np.array([[-7.5, -7.5], [-8.5, 7.5], [-2.5, 10], [5, 2.5]])
-features = np.array(
-    [
-        [-7.5, -7.5],
-        [-8.5, 7.5],
-        [-2.5, 10],
-        [5, 2.5],
-        [-8.5, -8.5],
-        [-9.5, 6.5],
-        [-3.5, 9],
-        [4, 1.5],
-        [-6.5, -6.5],
-        [-7.5, 8.5],
-        [-1.5, 11],
-        [6, 3.5],
-    ]
-)
-estimated_labels = np.array(
-    [
-        2,
-        1,
-        3,
-        4,
-        2,
-        1,
-        3,
-        4,
-        2,
-        1,
-        3,
-        4,
-    ]
-)
- """
 
 
 def visualise(features, estimated_labels, cluster_centers):
@@ -61,7 +22,7 @@ def k_means(featureArray: np.ndarray, nGroups):
     estimated_labels = np.array(list(range(len(featureArray))))
     cluster_centers = np.random.rand(nGroups, 2) * 2
     iterations = 0
-    while iterations < 10:
+    while iterations < 10000:
         prev_cluster_centers = cluster_centers.copy()
         for i, coord in enumerate(featureArray):
             temp_center_distances = []
@@ -83,9 +44,33 @@ def k_means(featureArray: np.ndarray, nGroups):
         iterations += 1
         if (prev_cluster_centers == cluster_centers).all():
             break
-        visualise(features, estimated_labels, cluster_centers)
-        plt.show()
     return estimated_labels, cluster_centers
 
 
 estimated_labels, cluster_centers = k_means(features, 4)
+
+visualise(features, estimated_labels, cluster_centers)
+
+
+def evaluate(featureArray, estimated_labels, cluster_centers):
+    sum = 0
+    count = 0
+    for i, center in enumerate(cluster_centers):
+        if np.any(estimated_labels == i):
+            for coord in featureArray[estimated_labels == i]:
+                sum += abs(math.hypot((coord[0] - center[0]), (coord[1] - coord[1])))
+                count += 1
+
+    return sum / count
+
+
+evaluate(features, estimated_labels, cluster_centers)
+
+evaluation = []
+k_times = 10
+for k in range(k_times):
+    estimated_labels, cluster_centers = k_means(features, k + 1)
+
+    evaluation.append(evaluate(features, estimated_labels, cluster_centers))
+plt.figure(), plt.plot(range(k_times), evaluation)
+plt.show()
